bafeee1dea90b716884457237895376a0c0d1244
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-04-08 16:06:52 -0700

    defines card component and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Recipe from './components/recipe';
-import './stylesheets/recipes.css';
+import './stylesheets/recipes.scss';
 
 class App extends Component {
   render() {
     return (
       <>
+      <div className="App">
         <Recipe 
           title='pasta'
           ingredients={['flour', 'water']}
+          instructions='mix together'
+          img='spaghetti.jpg'
         />
+      </div>
       </>
     );
   }
--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import '../stylesheets/components.css';
+import '../stylesheets/card.scss';
 
 class Recipe extends Component {
     render() {
 
-        const {title} = this.props;
+        const {title, img, instructions} = this.props;
         const ingredients = this.props.ingredients.map( (ingredient, idx) => (
             <li key={idx}>{ingredient}</li>
         ));
 
         return (
             <>
-                <div>{title}</div>
-                <ul>
-                    {ingredients}
-                </ul>
+            <div className="card">
+                <div className="card-img">
+                    <img src={img} alt={title} />
+                </div>
+                <div className="card-content">
+                    <h3 className="recipe-title">{title}</h3>
+                    <h4>Ingredients:</h4>
+                    <ul>
+                        {ingredients}
+                    </ul>
+                    <h4>
+                        Instructions:
+                    </h4>
+                    <p>
+                        {instructions}
+                    </p>
+                </div>
+            </div>
             </>
         );
     }
